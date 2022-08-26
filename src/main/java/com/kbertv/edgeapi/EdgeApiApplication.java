5dfdaec1345624806484272cb7ced8b42018b94c
Thao Privat <thuthaonguyen@thaosmbp.localdomain>
2022-08-27 01:16:52 +0200

    add basic project

---
+++ src/main/java/com/kbertv/edgeapi/EdgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.kbertv.edgeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbertv/edgeapi/EdgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbertv.edgeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
