20e83a59c8cb56d95b72db940e8677706a6786a1
ththng <t.t.nguyen@live.de>
2022-09-01 19:32:06 +0200

    set up minimal project for api gateway KBE-33 KBE-45

---
+++ src/main/java/com/kbertv/edgeapi/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.kbertv.edgeapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
