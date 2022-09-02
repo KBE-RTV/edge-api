9a3c3c9f0ba1e1fb5ccb42afaa2fb64dc2a7b46e
ththng <t.t.nguyen@live.de>
2022-09-02 13:29:29 +0200

    fix: make keycloak work with docker KBE-21

--- src/main/java/com/kbertv/edgeapi/Controller.java
+++ src/main/java/com/kbertv/edgeapi/Controller.java
@@ -1,4 +1,4 @@
-package com.kbertv.edgeapi.controller;
+package com.kbertv.edgeapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
